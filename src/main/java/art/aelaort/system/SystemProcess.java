c9f4ebae33de9f96d007733285d9349e7a852f70
ort <orthodoxplus@ya.ru>
2024-08-13 04:19:11 +0300

    SystemProcess cmd

--- src/main/java/art/aelaort/system/SystemProcess.java
+++ src/main/java/art/aelaort/system/SystemProcess.java
@@ -6,13 +6,46 @@ import org.springframework.stereotype.Component;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 @Component
 public class SystemProcess {
+	private final Set<String> knownCommands = Set.of("mvn", "yarn", "docker");
+	private final Set<String> cmdCommands = Set.of("mvn", "yarn");
+
+	private String fixCmdCall(String command) {
+		String first = StringUtils.split(command)[0];
+		if (cmdCommands.contains(first)) {
+			return "cmd /c " + command;
+		}
+		if (knownCommands.contains(first)) {
+			return command;
+		}
+
+		Response response = callProcess("where /f " + first);
+		if (response.exitCode() == 0) {
+			String[] split = response.stdout().split("\"");
+			if (split.length >= 1) {
+				if (matchExt(split, ".exe")) {
+					return command;
+				} else if (matchExt(split, ".cmd")) {
+					return "cmd /c " + command;
+				}
+			}
+		}
+		return command;
+	}
+
+	private boolean matchExt(String[] paths, String extension) {
+		return Stream.of(paths).anyMatch(s -> s.endsWith(extension));
+	}
+
 	public void callProcessForBuild(String command, Path dir) {
 		try {
-			Process p = new ProcessBuilder(StringUtils.split(command))
+			String[] commandArray = StringUtils.split(fixCmdCall(command));
+			Process p = new ProcessBuilder(commandArray)
 					.inheritIO()
 					.directory(dir.toFile())
 					.start();
