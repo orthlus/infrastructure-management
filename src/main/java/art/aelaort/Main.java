e599f8a38b03ceb018daec6b5f61f724b854e1d6
ort <orthodoxplus@ya.ru>
2024-08-14 00:50:47 +0300

    jsch - new config for RegisterReflectionForBinding

--- src/main/java/art/aelaort/Main.java
+++ src/main/java/art/aelaort/Main.java
@@ -2,8 +2,10 @@ package art.aelaort;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"art.aelaort", "com.jcraft.jsch"})
 public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
