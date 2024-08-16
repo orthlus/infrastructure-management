bb7b84a6f6a7f19e5aa72925260c5701d24810b8
ort <orthodoxplus@ya.ru>
2024-08-16 16:31:12 +0300

    lombok

--- src/main/java/art/aelaort/models/servers/ServiceDto.java
+++ src/main/java/art/aelaort/models/servers/ServiceDto.java
@@ -1,20 +1,16 @@
 package art.aelaort.models.servers;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ServiceDto {
 	private String service;
 	private String ymlName;
 	private String dockerName;
-
-	public ServiceDto(String service, String ymlName, String dockerName) {
-		this.service = service;
-		this.ymlName = ymlName;
-		this.dockerName = dockerName;
-	}
 
 	public ServiceDto(String service, String ymlName) {
 		this.service = service;
