b57b439cefd12466d8d38f938a8de121b3379574
ort <orthodoxplus@ya.ru>
2024-08-26 16:11:39 +0300

    clean

--- src/main/java/art/aelaort/make/Project.java
+++ src/main/java/art/aelaort/make/Project.java
@@ -2,16 +2,13 @@ package art.aelaort.make;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.With;
 
 @Getter
 @Builder
 public class Project {
-	@With
 	private String name;
 	private Integer id;
 	private boolean hasGit;
-	@With
 	private boolean hasJooq;
 	private boolean isMavenBuildForLocal;
 }
