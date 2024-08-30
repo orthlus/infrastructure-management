e078344b096d8fefb614e09f024bc91aa3f21ba5
ort <orthodoxplus@ya.ru>
2024-08-31 01:39:12 +0300

    graal config

--- src/main/java/art/aelaort/ReflectionConfig.java
+++ src/main/java/art/aelaort/ReflectionConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RegisterReflectionForBinding({
 		art.aelaort.models.build.Job.class,
+		art.aelaort.models.build.PomModel.class,
+		art.aelaort.models.build.PomModel.Properties.class,
 		art.aelaort.models.servers.Server.class,
 		art.aelaort.models.servers.yaml.TabbyFile.class,
 		art.aelaort.models.servers.yaml.CustomFile.class,
