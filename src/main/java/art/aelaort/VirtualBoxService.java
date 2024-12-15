74bb82254314f55d9dc66899a32d22e3b113f067
ort <orthodoxplus@ya.ru>
2024-12-15 19:53:48 +0300

    VirtualBoxService - check args fix

--- src/main/java/art/aelaort/VirtualBoxService.java
+++ src/main/java/art/aelaort/VirtualBoxService.java
@@ -17,25 +17,29 @@ public class VirtualBoxService {
 	private List<String> virtualboxNames;
 
 	public void up(String... args) {
-		checkArgs(args);
-		systemProcess.callProcessInheritIO("vboxmanage startvm %s --type headless".formatted(nameById(args)));
+		if (existsArgs(args)) {
+			systemProcess.callProcessInheritIO("vboxmanage startvm %s --type headless".formatted(nameById(args)));
+		}
 	}
 
 	public void pause(String... args) {
-		checkArgs(args);
-		systemProcess.callProcessInheritIO("vboxmanage controlvm %s savestate".formatted(nameById(args)));
+		if (existsArgs(args)) {
+			systemProcess.callProcessInheritIO("vboxmanage controlvm %s savestate".formatted(nameById(args)));
+		}
 	}
 
 	public void stop(String... args) {
-		checkArgs(args);
-		systemProcess.callProcessInheritIO("vboxmanage controlvm %s acpipowerbutton".formatted(nameById(args)));
+		if (existsArgs(args)) {
+			systemProcess.callProcessInheritIO("vboxmanage controlvm %s acpipowerbutton".formatted(nameById(args)));
+		}
 	}
 
-	private void checkArgs(String... args) {
+	private boolean existsArgs(String... args) {
 		if (args.length <= 1) {
-			log("at least 2 args required. list vms:");
 			log(list());
+			return false;
 		}
+		return true;
 	}
 
 	private String nameById(String... args) {
