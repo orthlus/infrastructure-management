7880d9e4c81bc2480f0197ac90daa9dc85405269
ort <orthodoxplus@ya.ru>
2025-03-20 13:51:16 +0300

    tests for graalvm

---
+++ src/test/java/art/aelaort/TestForNativeImage.java
@@ -0,0 +1,29 @@
+package art.aelaort;
+
+import art.aelaort.servers.providers.K8sClusterProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doNothing;
+
+@SpringBootTest
+public class TestForNativeImage {
+	@MockitoBean
+	private Entrypoint entrypoint;
+	@Autowired
+	private K8sClusterProvider k8sClusterProvider;
+
+	@BeforeEach
+	public void initAll() {
+		doNothing().when(entrypoint).run("");
+	}
+
+	@Test
+	public void testAllForReflection() {
+		assertThat(k8sClusterProvider.getClusters()).isNotEmpty();
+	}
+}
