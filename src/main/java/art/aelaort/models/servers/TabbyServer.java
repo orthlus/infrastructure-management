1dc94e0cecac858d2f706fd8bd10f6ee52b7b62e
ort <orthodoxplus@ya.ru>
2025-04-03 02:04:48 +0300

    clean models

--- src/main/java/art/aelaort/models/servers/TabbyServer.java
+++
@@ -1,4 +0,0 @@
-package art.aelaort.models.servers;
-
-public record TabbyServer(String name, String host, String keyPath, int port) {
-}
--- src/main/java/art/aelaort/models/ssh/SshServer.java
+++
@@ -1,4 +0,0 @@
-package art.aelaort.models.ssh;
-
-public record SshServer(String host, String fullKeyPath, int port, String serverDirName) {
-}
